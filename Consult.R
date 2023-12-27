02136eed6ae89224b1cebc30eb343002c3c87df1
ccristbo <ccristbo@gmail.com>
2023-12-27 00:53:41 -0500

    data actualizada que permite entrada de datos

--- Consult.R
+++ Consult.R
@@ -1,72 +1,63 @@
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
-library(readr);library(readxl);library(openxlsx);library(haven)
-library(data.table);library(scales);library(lattice)
-library(ggplot2);library(plot3D);library(plotly)
-library(reshape);library(reshape2)
-library(tidyverse);library(dplyr)
-library(forecast);library(dynlm)
-library(aTSA);library(olsrr)
-library(Rmisc);library(moderndive)
-library(magrittr);library(stringr)
-library(lubridate);library(stringi)
-library(lmtest);library(sandwich)
-library(xtable);library(ggExtra)
+library(readr)
+library(ggplot2)
+library(plotly)
+library(reshape)
+library(reshape2)
+library(tidyverse)
+library(dplyr)
+library(forecast)
+library(dynlm)
+library(aTSA)
+library(olsrr)
+library(Rmisc)
+library(moderndive)
+library(magrittr)
+library(stringr)
+library(lubridate)
+library(stringi)
+library(lmtest)
+library(sandwich)
+library(ggExtra)
 library(formattable)
 library(DT)
- 
 
-database <- read_excel("CUENTAS POLPER FINAL.xlsx", sheet="CONSOLIDADO_2023")
+pagos <- read_csv("pagos.csv", col_types = cols(
+  FECHA = col_date(format = "%m/%d/%Y"),
+  DIA_DEL_COBRO = col_datetime(format = "%m/%d/%Y %H:%M"),
+  DIA_DEL_PAGO = col_character()
+))
+pacientes <- read.csv(archivo_csv_pacientes, stringsAsFactors = FALSE)
 
-nom1 <-  c("ID","NOMBRE","PAIS","CIUDAD","CEL","TERAPIA","FRECUENCIA")
-nom2 <-  c("FECHA","ESTADO","DIA DEL COBRO","DIA DEL PAGO","VALOR_CONSULTA","ABONO","MONEDA","PAGO","OBSERVACIONES","FORMA DE PAGO")
-database <- database %>%
-  mutate(ABONO=as.numeric(ABONO),FECHA=as.Date(FECHA))
-# suma del total generado cada dia en cada moneda
-db.agg   <- database %>% 
-  group_by(FECHA,MONEDA) %>% 
-                         dplyr::summarise(TOTAL=sum(VALOR_CONSULTA,na.rm = T))
-# suma del abono y respectivo total de consultas+ultima consulta
-db.cuentas <- database %>%
-  group_by(ID) %>% 
-                           dplyr::summarise(TOTAL=sum(VALOR_CONSULTA,na.rm = T),
-                           ABONO=sum(ABONO,na.rm = T))
-# estima el saldo total
-# revisar por que 
-db.saldo <- db.cuentas %>% 
-  mutate(SALDO=ABONO-TOTAL) 
+database = pagos %>%
+  left_join(pacientes)
 
-#nom1 definido al principio
-db.nom   <- database %>% 
-  dplyr::select(nom1) %>%
-  dplyr::distinct()
+nom1 <- c("ID", "NOMBRE", "PAIS", "CIUDAD", "CEL", "TERAPIA", "FRECUENCIA")
+nom2 <- c(
+  "FECHA", "ESTADO", "DIA DEL COBRO", "DIA DEL PAGO", "VALOR_CONSULTA",
+  "ABONO", "MONEDA", "PAGO", "OBSERVACIONES", "FORMA DE PAGO"
+)
+database <- database %>% mutate(ABONO = as.numeric(ABONO), FECHA = as.Date(FECHA))
+db.agg <- database %>% group_by(FECHA, MONEDA) %>%
+  dplyr::summarise(TOTAL = sum(VALOR_CONSULTA, na.rm = TRUE))
+db.cuentas <- database %>% group_by(ID) %>%
+  dplyr::summarise(
+    TOTAL = sum(VALOR_CONSULTA, na.rm = TRUE),
+    ABONO = sum(ABONO, na.rm = TRUE)
+  )
+db.saldo <- db.cuentas %>% mutate(SALDO = ABONO - TOTAL)
+db.nom <- database %>% dplyr::select(nom1) %>% dplyr::distinct()
+db.cue <- db.nom %>% left_join(db.saldo, by = "ID")
+db.deu <- db.cue %>% dplyr::filter(SALDO < 0) %>% arrange(NOMBRE)
+db.ald <- db.cue %>% dplyr::filter(SALDO >= 0) %>% arrange(NOMBRE)
 
-# adjunta la info del paciente
-db.cue   <- db.nom %>%
-  left_join(db.saldo,by="ID") 
-
-# grupo deudores saldo negativo
-db.deu   <- db.cue %>% 
-  dplyr::filter(SALDO<0) %>% 
-  arrange(NOMBRE)
-
-# grupo deudores saldo postivo No deben
-db.ald   <- db.cue %>% 
-  dplyr::filter(SALDO>=0) %>% 
-  arrange(NOMBRE) 
-
-datosCont <- function(){
+datosCont <- function() {
   sidebarLayout(
-      textInput("personID", "Digite ID:", ""),
-      actionButton("searchButton", "Buscar")
+    textInput("personID", "Digite ID:", ""),
+    actionButton("searchButton", "Buscar")
   )}
-# datosFech <- function() {
-#   sidebarLayout(
-#     textInput("personID", "Digite ID:", ""),
-#     actionButton("searchButton", "Buscar")
-#   )
-# }
 # Define the UI
 ui <- navbarPage(
   tags$head(
@@ -81,74 +72,58 @@ ui <- navbarPage(
   ),
   title = div(strong("POLPER")),
   theme = shinytheme("flatly"),
-    tabsetPanel(type = "tabs",
-                tabPanel("Registrar Usuario",
-                  sidebarPanel(
-                  textInput("name", "Enter Your Name:", ""),
-                  numericInput("age", "Enter Your Age:", value = NULL),
-                  actionButton("submit", "Submit"),
-                  downloadButton("export", "Export to Excel")
-                )
-                ),          
-      tabPanel("Informacion individual",
-               datosCont(),
-               h3("Datos de contacto"),
-               dataTableOutput("view1"),
-               br(),
-               br(),
-               h3("Registro historico"),
-               dataTableOutput("view2"),
-               br(),
-               verbatimTextOutput("searchResult")
-               ), 
-      #
-      tabPanel("Agregado mensual",
-               br(),
-               # Plotly scatter plot
-               plotlyOutput("scatter_plot_aggregated"),
-               br(),
-               # Data table
-               dataTableOutput("vfecha")
-      ),
-      #
-      # tabPanel("Agregado mensual",
-      #          br(),
-      #          # p("Digite el mes que desea consultar:"),
-      #          dataTableOutput("vfecha")
-      #          ),
-      tabPanel("Personas al dia",
-               br(),
-               # p("Digite el mes que desea consultar:"),
-               dataTableOutput("valdia")
-               ),
-      tabPanel("Personas por pagar",
-               br(),
-               # p("Digite el mes que desea consultar:"),
-               dataTableOutput("vporpagar")
-               )
+  tabsetPanel(
+    type = "tabs",
+    tabPanel(
+      "Informacion individual",
+      datosCont(),
+      h3("Datos de contacto"),
+      dataTableOutput("view1"),
+      br(),
+      br(),
+      h3("Registro historico"),
+      dataTableOutput("view2"),
+      br(),
+      verbatimTextOutput("searchResult")
+    ),
+    tabPanel(
+      "Agregado mensual",
+      br(),
+      # p("Digite el mes que desea consultar:"),
+      dataTableOutput("vfecha")
+    ),
+    tabPanel(
+      "Personas al dia",
+      br(),
+      # p("Digite el mes que desea consultar:"),
+      dataTableOutput("valdia")
+    ),
+    tabPanel(
+      "Personas por pagar",
+      br(),
+      # p("Digite el mes que desea consultar:"),
+      dataTableOutput("vporpagar")
     )
   )
-# Define the server
+)
 server <- function(input, output) {
   output$view1 <- renderDataTable({
     ID.i <- input$personID
     if (ID.i %in% db.cue$ID) {
-      consulta = db.cue %>%
-        dplyr::filter(ID==ID.i) %>% 
-        dplyr::distinct()
+      consulta = db.cue %>% dplyr::filter(ID == ID.i) %>% dplyr::distinct()
       consulta
     } else {
-    consulta <- data.frame(Resultado="Persona no inscrita")
+      consulta <- data.frame(Resultado = "Persona no inscrita")
     }
   })
   output$view2 <- renderDataTable({
     ID.i <- input$personID
     if (ID.i %in% database$ID) {
-        consulta1 = database %>% dplyr::filter(ID==ID.i) %>% 
-                                 dplyr::select(nom2)
+      consulta1 = database %>% dplyr::filter(ID == ID.i) %>%
+        dplyr::select(nom2)
       consulta1
     } else {
-      consulta1 <- data.frame(Resultado="Persona no inscrita")
+      consulta1 <- data.frame(Resultado = "Persona no inscrita")
     }
   })
   output$vfecha <- renderDataTable({
@@ -165,27 +140,6 @@ server <- function(input, output) {
     db.ppag <- db.deu
     db.ppag
   })
-  output$scatter_plot_aggregated <- renderPlotly({
-    # Your plot_ly code here, using aggregated data (e.g., db.agg)
-    plot_ly(db.agg, x = ~FECHA, y = ~TOTAL, color = ~MONEDA, type = 'scatter', mode = 'lines+markers') %>%
-      layout(title = "Aggregated Monthly Data",
-             xaxis = list(title = "Date"),
-             yaxis = list(title = "Total Consultation Value"),
-             showlegend = TRUE)
-  })
-  
-  # observeEvent(input$searchButton, {
-  #   ID.i <- input$personID
-  #   if (ID.i %in% database$ID) {
-  #     consulta1 = database %>% dplyr::filter(ID==ID.i) %>% 
-  #                              dplyr::select(nom2)
-  #     r <- (sum(consulta1$VALOR_CONSULTA,na.rm = T)-sum(consulta1$ABONO,na.rm = T))
-  #     color = ifelse(r > 0, "forestgreen", ifelse(r <= 0, "red", "black"))
-  #     output$searchResult <- renderText(paste("Saldo",r))
-  #   } else {
-  #     output$searchResult <- renderText(paste("No hay resultados"))
-  #   }
-  # })
   observeEvent(input$submit, {
     # Add user input to the data frame
     data <<- rbind(data, data.frame(Name = input$name, Age = input$age))
@@ -201,8 +155,7 @@ server <- function(input, output) {
     },
     content = function(file) {
       write_xlsx(data, file)
-    }
-  )
+    })
   
   output$scatter_plot <- renderPlotly({
     plot_ly(data, x = ~Age, y = ~Name, type = 'scatter', mode = 'markers')
@@ -212,4 +165,3 @@ server <- function(input, output) {
 
 # Run the Shiny app
 shinyApp(ui, server)
-
